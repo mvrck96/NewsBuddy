97611d60a2383e53c99543dc51c026a5980517f1
Ivan <maverickmsk1@gmail.com>
2023-09-23 15:08:33 +0300

    HF connection check added

---
+++ NewsBuddy/model_service/tools/utils.py
@@ -0,0 +1,21 @@
+import requests
+
+from requests import ConnectionError
+from tools.logger import service_logger as logger
+
+
+def check_hugging_face_connection(url: str) -> bool:
+    """Checks if HuggingFace API is accessible
+
+    Args:
+        url (str): HuggingFace model url
+
+    Returns:
+        bool: Success flag
+    """
+    try:
+        response = requests.get(url)
+        logger.debug(response.status_code)
+        return True
+    except ConnectionError:
+        return False
