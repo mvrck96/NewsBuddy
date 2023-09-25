61d9e93ba01c9d282d1c1f54c67fa96ba12639a2
Ivan <maverickmsk1@gmail.com>
2023-09-25 13:44:29 +0300

    log format changed

--- NewsBuddy/model_service/tools/logger.py
+++ NewsBuddy/model_service/tools/logger.py
@@ -1,6 +1,15 @@
+import os
 import sys
 
 from loguru import logger
+
+
+LOG_FORMAT = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+    "<level>{level: <8}</level> | "
+    f"<blue>{os.getpid()}</blue> | "
+    "<cyan>{name}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+)
 
 
 def create_logger(serialized: bool = False) -> logger:
@@ -11,7 +20,7 @@ def create_logger(serialized: bool = False) -> logger:
     @return [logger]: Logger
     """
     logger.remove()
-    logger.add(sys.stdout, serialize=serialized)
+    logger.add(sys.stdout, serialize=serialized, format=LOG_FORMAT)
     logger.info(f"Logger initialized, structured logs: {serialized}")
     return logger
 
