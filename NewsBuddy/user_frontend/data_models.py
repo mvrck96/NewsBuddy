91a956230db2c34a0451a2bdfeac971c4d21ae71
Ivan <maverickmsk1@gmail.com>
2023-09-30 20:07:30 +0300

    added alphavantage themes

---
+++ NewsBuddy/user_frontend/data_models.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class AlphavantageTopics(str, Enum):
+    """"""
+
+    EARNINGS = "earnings"
+    IPO = "ipo"
+    MERGERS_AND_ACQUSITIONS = "mergers_and_acqusitions"
+    FINANCIAL_MARKETS = "financial_markets"
+    FINANCE = "finance"
+    MANUFACTURING = "manufacturing"
+    REAL_ESTATE = "real_estate"
+    RETAIL_WHOLESAIL = "retail_wholesail"
+    TECHNOLOGY = "technology"
+    ECONOMY_MACRO = "economy_macro"
+    ECONOMY_FISCAL = "economy_fiscal"
+    ECONOMY_MONETARY = "economy_monetary"
+    LIFE_SCIENCES = "life_sciences"
