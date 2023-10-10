acf2c8b311833d2da2e43f093394b74d5d71977a
Ivan <maverickmsk1@gmail.com>
2023-10-10 22:14:16 +0300

    api manager request added

--- NewsBuddy/user_frontend/data_models.py
+++ NewsBuddy/user_frontend/data_models.py
@@ -1,7 +1,17 @@
+from typing import List, Optional
 from enum import Enum
 
+from pydantic import BaseModel, conint
+
+
+class Predict(BaseModel):
+    """Model for user text inference."""
+
+    text: str
+
+
 class AlphavantageTopics(str, Enum):
-    """"""
+    """Possible topics in"""
 
     EARNINGS = "earnings"
     IPO = "ipo"
@@ -16,3 +26,11 @@ class AlphavantageTopics(str, Enum):
     ECONOMY_FISCAL = "economy_fiscal"
     ECONOMY_MONETARY = "economy_monetary"
     LIFE_SCIENCES = "life_sciences"
+
+
+class ApiManagerRequest(BaseModel):
+    """Request to api manager srevice."""
+
+    tickers: Optional[List[str]] = None
+    topics: Optional[List[AlphavantageTopics]] = None
+    limit: conint(ge=1, le=50) = 10
