722ef09dc48ea911d6e6c931fdd208114272cc04
Ivan <maverickmsk1@gmail.com>
2023-10-17 22:53:35 +0300

    urls chnaged, minor ref

--- NewsBuddy/user_frontend/main.py
+++ NewsBuddy/user_frontend/main.py
@@ -34,13 +34,10 @@ def intro():
     st.sidebar.success("Select option above :arrow_up:")
 
     st.markdown(
-    """
+        """
         # Welocome to NewsBuddy !
         
-        > **Stock news analysis made for people**
-
-        
-
+        **Stock news analysis made for people**
     """
     )
 
@@ -59,7 +56,7 @@ def user_input():
         payload = Predict(text=user_news).json()
         try:
             response = session.post(
-                service_settings.model_service_url, payload
+                service_settings.model_service_url + "/predict", payload
             ).json()
             st.json(response)
         except requests.exceptions.ConnectionError as e:
@@ -74,28 +71,29 @@ def user_based_feed():
 
     with st.form("news_feed_settings"):
         tickers = st.multiselect(
-            label="Select tickers", 
-            options=["AAPL", "MSFT", "TSLA", "NVDA", "GOOGL"]
+            label="Select tickers",
+            options=["AAPL", "MSFT", "TSLA", "NVDA", "GOOGL"],
         )
         tickers = tickers if len(tickers) > 0 else None
 
         topics = st.multiselect(
-            label="Select topics", options=[i.value for i in AlphavantageTopics]
+            label="Select topics",
+            options=[i.value for i in AlphavantageTopics]
         )
         topics = topics if len(topics) > 0 else None
-        
+
         limit = st.slider("Number of news", 0, 50, 10, 1)
 
         if st.form_submit_button("Analyze latest news !"):
             api_man_payload = ApiManagerRequest(
-                tickers=tickers,
-                topics=topics,
-                limit=limit
+                tickers=tickers, topics=topics, limit=limit
             ).json()
             st.json(api_man_payload)
             try:
-                api_man_resp = requests.post(service_settings.api_manager_url,
-                                            api_man_payload).json()
+                api_man_resp = requests.post(
+                    service_settings.api_manager_url + "/user-news",
+                    api_man_payload,
+                ).json()
                 st.json(api_man_resp)
 
                 titles = [n["title"] for n in api_man_resp["feed"]]
@@ -104,8 +102,9 @@ def user_based_feed():
                     payload = Predict(text=title).json()
                     try:
                         response = session.post(
-                                service_settings.model_service_url, payload
-                            ).json()
+                            service_settings.model_service_url + "/predict",
+                            payload,
+                        ).json()
                         st.write(title)
                         st.json(response)
 
@@ -115,7 +114,7 @@ def user_based_feed():
 
             except requests.exceptions.ConnectionError as e:
                 st.error("Can't connect to api manager service !")
-                service_logger.debug(e)    
+                service_logger.debug(e)
 
 
 def latest_feed():
